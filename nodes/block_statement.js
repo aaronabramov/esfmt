0ec57a0a4066c8079f28e6b8a7fbc263a538dc03
Dmitrii Abramov <dabramov@yahoo-inc.com>
2015-09-08 18:39:37 -0700

    Cleanup

--- nodes/block_statement.js
+++ nodes/block_statement.js
@@ -21,37 +21,6 @@ export function format(node, context, recur) {
     result += formatBlock(node, context, recur);
     context.indentOut();
 
-    // if (node.body.length) {
-    //     // to hold previous node
-    //     let previous;
-    //
-    //     context.indentIn();
-    //     result += '\n';
-    //
-    //     result += node.body.map((child) => {
-    //         let childResult = '';
-    //
-    //         if (previous && newlines.extraNewLineBetween(previous, child)) {
-    //             childResult += '\n';
-    //         }
-    //
-    //         childResult += context.getIndent()
-    //             + recur(child)
-    //             + utils.getLineTerminator(child);
-    //
-    //         // current becomev previous
-    //         previous = child;
-    //
-    //         return childResult;
-    //     }).join('\n');
-    //
-    //     result +='\n';
-    //
-    //     context.indentOut();
-    //     result += context.getIndent();
-    //
-    // }
-
     result += '\n' + context.getIndent() + '}';
 
     return result;
