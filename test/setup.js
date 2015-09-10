df5f2d1a673341f66a221b0fece77c170e610f8d
Dmitrii Abramov <dabramov@yahoo-inc.com>
2015-09-09 23:07:22 -0700

    es6 coverage

---
+++ test/setup.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var babel = require('babel');
+var istanbul = require('istanbul');
+var COV_VAR = '______coverage______';
+
+var instrumenter = new istanbul.Instrumenter({
+    coverageVariable: COV_VAR
+});
+
+require.extensions['.js'] = function(module, filename) {
+    var src = fs.readFileSync(filename, {encoding: 'utf8'});
+
+    if (!filename.match(/(node_modules)/)) {
+        src = babel.transform(src, {sourceMaps: 'inline', filename: filename}).code;
+        if (!filename.match(/.*_test\.js$/)) {
+            src = instrumenter.instrumentSync(src, filename);
+        }
+    }
+
+    module._compile(src, filename);
+};
+
+after(function() {
+    var collector = new istanbul.Collector();
+    var reporter = new istanbul.Reporter();
+
+    collector.add(global[COV_VAR]);
+
+    reporter.add('json');
+    reporter.add('lcov');
+
+    reporter.write(collector, true, function() {
+        console.log(arguments);
+        console.log('DONE');
+    });
+});
