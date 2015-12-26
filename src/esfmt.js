402ed5daf34f9d63ce9235fe028da663eacd4e43
maximderbin <babybeasimple@gmail.com>
2015-12-25 18:01:38 -0600

    Sort imports

--- src/esfmt.js
+++ src/esfmt.js
@@ -5,15 +5,28 @@ import * as ArrowFunctionExpression from './nodes/arrow_function_expression';
 import * as AssignmentExpression from './nodes/assignment_expression';
 import * as BinaryExpression from './nodes/binary_expression';
 import * as BlockStatement from './nodes/block_statement';
+import * as BreakStatement from './nodes/break_statement';
 import * as CallExpression from './nodes/call_expression';
 import * as CatchClause from './nodes/catch_clause';
+import * as ClassBody from './nodes/class_body';
+import * as ClassDeclaration from './nodes/class_declaration';
 import * as ConditionalExpression from './nodes/conditional_expression';
+import * as DoWhileStatement from './nodes/do_while_statement';
+import * as EmptyStatement from './nodes/empty_statement';
+import * as ExportDefaultDeclaration from './nodes/export_default_declaration';
+import * as ExportNamedDeclaration from './nodes/export_named_declaration';
+import * as ExportSpecifier from './nodes/export_specifier';
 import * as ExpressionStatement from './nodes/expression_statement';
+import * as ForInStatement from './nodes/for_in_statement';
+import * as ForStatement from './nodes/for_statement';
 import * as FunctionDeclaration from './nodes/function_declaration';
 import * as FunctionExpression from './nodes/function_expression';
 import * as Identifier from './nodes/identifier';
 import * as IfStatement from './nodes/if_statement';
 import * as ImportDeclaration from './nodes/import_declaration';
+import * as ImportDefaultSpecifier from './nodes/import_default_specifier';
+import * as ImportNamespaceSpecifier from './nodes/import_namespace_specifier';
+import * as ImportSpecifier from './nodes/import_specifier';
 import * as JSXAttribute from './nodes/jsx_attribute';
 import * as JSXClosingElement from './nodes/jsx_closing_element';
 import * as JSXElement from './nodes/jsx_element';
@@ -23,39 +36,26 @@ import * as JSXOpeningElement from './nodes/jsx_opening_element';
 import * as Literal from './nodes/literal';
 import * as LogicalExpression from './nodes/logical_expression';
 import * as MemberExpression from './nodes/member_expression';
+import * as MethodDefinition from './nodes/method_definition';
 import * as NewExpression from './nodes/new_expression';
 import * as ObjectExpression from './nodes/object_expression';
 import * as Program from './nodes/program';
 import * as Property from './nodes/property';
+import * as RestElement from './nodes/rest_element';
 import * as ReturnStatement from './nodes/return_statement';
-import * as TryStatement from './nodes/try_statement';
-import * as TemplateLiteral from './nodes/template_literal';
+import * as SpreadElement from './nodes/spread_element';
+import * as Super from './nodes/super';
+import * as SwitchCase from './nodes/switch_case';
+import * as SwitchStatement from './nodes/switch_statement';
 import * as TemplateElement from './nodes/template_element';
+import * as TemplateLiteral from './nodes/template_literal';
+import * as ThisExpression from './nodes/this_expression';
+import * as ThrowStatement from './nodes/throw_statement';
+import * as TryStatement from './nodes/try_statement';
 import * as UnaryExpression from './nodes/unary_expression';
 import * as UpdateExpression from './nodes/update_expression';
 import * as VariableDeclaration from './nodes/variable_declaration';
 import * as VariableDeclarator from './nodes/variable_declarator';
-import * as ImportDefaultSpecifier from './nodes/import_default_specifier';
-import * as ImportNamespaceSpecifier from './nodes/import_namespace_specifier';
-import * as ImportSpecifier from './nodes/import_specifier';
-import * as ForStatement from './nodes/for_statement';
-import * as ThisExpression from './nodes/this_expression';
-import * as ForInStatement from './nodes/for_in_statement';
-import * as DoWhileStatement from './nodes/do_while_statement';
-import * as ThrowStatement from './nodes/throw_statement';
-import * as ExportDefaultDeclaration from './nodes/export_default_declaration';
-import * as ExportNamedDeclaration from './nodes/export_named_declaration';
-import * as ExportSpecifier from './nodes/export_specifier';
-import * as EmptyStatement from './nodes/empty_statement';
-import * as ClassDeclaration from './nodes/class_declaration';
-import * as ClassBody from './nodes/class_body';
-import * as MethodDefinition from './nodes/method_definition';
-import * as RestElement from './nodes/rest_element';
-import * as Super from './nodes/super';
-import * as SpreadElement from './nodes/spread_element';
-import * as SwitchStatement from './nodes/switch_statement';
-import * as SwitchCase from './nodes/switch_case';
-import * as BreakStatement from './nodes/break_statement';
 
 
 import esprima from 'espree';
@@ -68,40 +68,33 @@ import invariant from './invariant';
 import '../polyfills/includes';
 
 const NODE_TYPES = {
-    BreakStatement,
-    SwitchCase,
-    SwitchStatement,
-    SpreadElement,
-    Super,
-    RestElement,
-    MethodDefinition,
-    ClassBody,
-    ClassDeclaration,
-    EmptyStatement,
-    ExportSpecifier,
-    ExportNamedDeclaration,
-    ExportDefaultDeclaration,
-    ThrowStatement,
-    DoWhileStatement,
-    ForInStatement,
-    ThisExpression,
-    ForStatement,
-    ImportNamespaceSpecifier,
-    ImportSpecifier,
     ArrayExpression,
     ArrowFunctionExpression,
     AssignmentExpression,
     BinaryExpression,
     BlockStatement,
+    BreakStatement,
     CallExpression,
     CatchClause,
+    ClassBody,
+    ClassDeclaration,
     ConditionalExpression,
+    DoWhileStatement,
+    EmptyStatement,
+    ExportDefaultDeclaration,
+    ExportNamedDeclaration,
+    ExportSpecifier,
     ExpressionStatement,
+    ForInStatement,
+    ForStatement,
     FunctionDeclaration,
     FunctionExpression,
     Identifier,
     IfStatement,
     ImportDeclaration,
+    ImportDefaultSpecifier,
+    ImportNamespaceSpecifier,
+    ImportSpecifier,
     JSXAttribute,
     JSXClosingElement,
     JSXElement,
@@ -111,19 +104,26 @@ const NODE_TYPES = {
     Literal,
     LogicalExpression,
     MemberExpression,
+    MethodDefinition,
     NewExpression,
     ObjectExpression,
     Program,
     Property,
+    RestElement,
     ReturnStatement,
+    SpreadElement,
+    Super,
+    SwitchCase,
+    SwitchStatement,
+    TemplateElement,
+    TemplateLiteral,
+    ThisExpression,
+    ThrowStatement,
     TryStatement,
     UnaryExpression,
     UpdateExpression,
     VariableDeclaration,
-    VariableDeclarator,
-    ImportDefaultSpecifier,
-    TemplateLiteral,
-    TemplateElement
+    VariableDeclarator
 };
 
 
