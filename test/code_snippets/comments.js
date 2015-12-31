bfc8b99aedb440400425eb6881ec89562d243451
Dmitrii Abramov <dabramov@yahoo-inc.com>
2015-12-30 20:10:34 -0800

    comments

--- test/code_snippets/comments.js
+++ test/code_snippets/comments.js
@@ -97,3 +97,124 @@ function a() {
     return b;
 } // b
 /* c */
+
+// input: in classes
+// skip
+/* abc */
+class A { // cde
+    /* abc */
+    constructor() { // d
+        super(); // a
+    } // l
+
+    // oeu
+    method() {
+        return 5; // b
+    } // abc
+    // cde
+} // ath
+// oeu
+// output:
+/* abc */
+class A { // cde
+    /* abc */
+    constructor() { // d
+        super(); // a
+    } // l
+
+    // oeu
+    method() {
+        return 5; // b
+    } // abc
+    // cde
+} // ath
+// oeu
+
+// input: in arrays
+// skip
+[a, /* b */ c /* d */]; // abc
+// output:
+[a, /* b */ c /* d */]; // abc
+
+// input: in function argument list
+// skip
+function a(a, /* b */ c /* rest */) {}
+a(a, /* b */ c /* rest */);
+// output:
+function a(a, /* b */ c /* rest */) {}
+
+a(a, /* b */ c /* rest */);
+
+// input: in objects
+let a = {
+    /* b */
+    a: function() {}, // cde
+    // abc
+    b: 'test test test' // abc
+    // a
+}; // a
+// b
+// output:
+let a = {
+    /* b */
+    a: function() {}, // cde
+    // abc
+    b: 'test test test' // abc
+    // a
+}; // a
+// b
+
+// input: trailing comment on the first line
+// skip
+const a = { // abc
+    b
+};
+// output:
+const a = { // abc
+    b
+};
+
+// input: trailing comment on the last line without ';'
+// skip
+const a = {
+    b
+} // a
+// b
+// output:
+const a = {
+    b
+}; // a
+// b
+
+// input: in variable declaration blocks
+// skip
+// aaaa
+let a = 5, // acb
+    // aaa
+    b = 5; // ddd
+// output:
+// aaaa
+let a = 5, // acb
+    // aaa
+    b = 5; // ddd
+
+// input: in multiline arrays
+// skip
+// config: {"max-len": 5}
+let a = [ // bbb
+    1, // cccc
+    2, // ddd
+    /* 234'4 */
+    3
+    // abc
+] // 999
+// 888
+// output:
+let a = [ // bbb
+    1, // cccc
+    2, // ddd
+    /* 234'4 */
+    3
+    // abc
+] // 999
+// 888
